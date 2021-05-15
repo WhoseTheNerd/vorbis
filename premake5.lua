6a5083f1e33995baf7cf43c76b5bc6e3aabf73c3
WhoseTheNerd <tutorialvideohd@gmail.com>
2021-05-15 15:02:55 +0300

    Add premake 5 file

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+project "vorbis"
+    kind "StaticLib"
+    language "C"
+    staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "lib/**.h",
+        "lib/**.c"
+    }
+    
+    removefiles 
+    {
+        "lib/psytune.c",
+        "lib/misc.c"
+    }
+
+    includedirs
+    {
+        "include",
+        "%{IncludeDir.ogg}"
+    }
+
+    defines
+    {
+        "_USE_MATH_DEFINES"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
